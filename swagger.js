5ecfd441dc3a5bdd6f61e7d63eb609636af4675a
abdullah-eldahrawy <abdullah.sherif2001@gmail.com>
2025-08-17 15:58:49 +0300

    corrected servers url in swagger options

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:4000/api', // change based on your base URL
+        url: 'https://signage-app-api.vercel.app//api', // change based on your base URL
       },
     ],
   },
