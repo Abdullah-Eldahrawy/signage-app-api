f8904a7c9a9d5c0bc8380a857e3494b04d1c2716
abdullah-eldahrawy <abdullah.sherif2001@gmail.com>
2025-08-24 14:53:14 +0300

    ficed signage request payload

--- controllers/bpm.controller.js
+++ controllers/bpm.controller.js
@@ -3,7 +3,7 @@ import https from 'https';
 
 export const forwardSignageRequest = async (req, res) => {
   try {
-    const apiUrl = "https://cpd-cp4ba.apps.automation.sumerge.com/bas/automationservices/rest/SB/SRF08_1/Signage%20Intake%20API/createSignageRequest";
+    const apiUrl = "https://cpd-cp4ba.apps.automation.sumerge.com/bas/automationservices/rest/SB/SR082_7/Signage%20Intake%20API/createSignageRequest";
 
     const username = 'fnadmin';
     const password = 'P@ssw00rd';
@@ -24,21 +24,23 @@ export const forwardSignageRequest = async (req, res) => {
 
     // Prepare the payload for CP4BA
     const cp4baPayload = {
-      fullName: signageRequest.fullName,
-      installationDate: signageRequest.installationDate,
-      installationAddress: signageRequest.installationAddress,
-      mountingSurface: signageRequest.mountingSurface,
-      signWidth: signageRequest.signWidth,
-      signHeight: signageRequest.signHeight,
-      displayType: signageRequest.displayType,
-      viewingDistance: signageRequest.viewingDistance,
-      primaryMessage: signageRequest.primaryMessage,
-      secondaryInfo: signageRequest.secondaryInfo,
-      colors: signageRequest.colors,
-      attachments: signageRequest.attachments || []
+      "signageRequest":{
+        fullName: signageRequest.fullName,
+        installationDate: signageRequest.installationDate,
+        installationAddress: signageRequest.installationAddress,
+        mountingSurface: signageRequest.mountingSurface,
+        signWidth: signageRequest.signWidth,
+        signHeight: signageRequest.signHeight,
+        displayType: signageRequest.displayType,
+        viewingDistance: signageRequest.viewingDistance,
+        primaryMessage: signageRequest.primaryMessage,
+        secondaryInfo: signageRequest.secondaryInfo,
+        colors: signageRequest.colors,
+        attachments: signageRequest.attachments || []
+      }
     };
 
-    const response = await axios.post(apiUrl, cp4baPayload, {
+    const response = await axios.post(apiUrl,cp4baPayload, {
       httpsAgent: agent,
       auth: {
         username,
